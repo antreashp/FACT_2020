4c228651a2af8751f467be458a97e17cf1a533aa
antreashp <antreashp@gmail.com>
2021-01-11 17:42:55 +0100

    fixed mlp, added main for sanity check

--- pyt_Code/base.py
+++ pyt_Code/base.py
@@ -2,21 +2,50 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-class MLP():
+class MLP(nn.Module):
     def __init__(self,shape):
+        super(MLP, self).__init__()
         self.shape = shape
-        self.weight_init = torch.nn.init.xavier_uniform()
+        # self.weight_init = torch.nn.init.xavier_uniform()
         # self.bias_init = 
         n = len(shape)
-        weights = nn.init.xavier_uniform(nn.Linear(self.shape[0], self.shape[1],bias = True))
-        self.model = nn.ModuleList([weights])
+        layer = nn.Linear(self.shape[0], self.shape[1],bias = True)
+        nn.init.xavier_uniform(layer.weight)
+        self.model = [layer]#nn.ModuleList([layer])
 
+        self.model.extend( [nn.LeakyReLU()])
     
-        for i in range(n-2):
-            weights= nn.init.xavier_uniform(nn.Linear(self.shape[i], self.shape[i]+1),bias = True)
+        for i in range(1,n-2):
 
-            self.model.extend( weights)
-        weights = nn.Linear(self.shape[-2], self.shape[-1],bias = True)
-        self.model.append(weights)
+            layer = nn.Linear(self.shape[i], self.shape[i+1],bias = True)
+            weights= nn.init.xavier_uniform(layer.weight)
+
+            self.model.extend( [layer])
+            self.model.extend( [nn.LeakyReLU()])
+            
+        layer = nn.Linear(self.shape[-2], self.shape[-1],bias = True)
+        nn.init.xavier_uniform(layer.weight)
+        # self.model = nn.ModuleList([layer])
+
+        # weights = nn.Linear(self.shape[-2], self.shape[-1],bias = True)
+        self.model.extend([layer])
+        # print(self.model)
+        self.model = nn.ModuleList(self.model)
+        # print(self.model)
     def forward(self,x):
-        x = self.model(x)
+        for f in self.model:
+            x = f(x)
+        return x
+
+
+if __name__ == "__main__":
+    shape = [10,20,30]
+    batch_size = 32
+    mlp = MLP(shape)
+    print(self.model)
+
+    x = torch.rand([batch_size,shape[0]])
+    print('input shape: ',x.shape)
+    out = mlp(x)
+    print('output shape: ',ou.shape)
+    
