9afcb5dd7b5451e7cfa8fa4db60a67b4290f0e4b
antreashp <antreashp@gmail.com>
2021-01-14 15:13:35 +0100

    bug in backward. implemented cycle. still debugging

--- pyt_Code/train_reg.py
+++ pyt_Code/train_reg.py
@@ -24,12 +24,13 @@ def train_reg(x, y,
     os.makedirs("Model")
     os.chdir("Model")
     
-    sys.stdout = open("train.txt", "w")
 
+    # sys.stdout = open("train.txt", "w")
+    print('blah')
     # Split the dataset
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25)
     x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size = 0.5)
-    
+    print('blah')
     # Evaluate baseline models
     model_lm = LinearRegression().fit(x_train, y_train)
     print("\nLM MSE: ", mean_squared_error(model_lm.predict(x_test), y_test), "\n")
@@ -56,8 +57,53 @@ def train_reg(x, y,
 
     decoder = MLP(decoder_shape)
     learner = MLP(learner_shape)
-    params = encoder.parameters + decoder.parameters + learner.parameters
+    # model_loss = mse(Y,pred)
+    # recon_loss = mse(X,recon)
+    # loss_op = model_loss + recon_weight *recon_loss 
+    print(type(encoder.parameters))
+    params = list(encoder.parameters()) + list(decoder.parameters()) + list(learner.parameters())
     optimizer = torch.optim.Adam(params,lr=learning_rate)
-    criterion = nn.MSELoss()
+    criterion = torch.nn.MSELoss()
 
+    best_epoch = 0 
+    best_loss = np.inf
+    epoch = 0
+    total_batch = int(n / batch_size)
+    while True:
 
+        # Stopping condition
+        if epoch - best_epoch > stopping_epochs and epoch > min_epochs:
+            break
+
+        # Run a training epoch
+        for i in range(total_batch):
+            x_batch, y_batch = bm.next_batch(batch_size = batch_size)
+            # summary, _ = sess.run([summary_op, train_op], feed_dict = {X: x_batch, Y: y_batch})
+            # train_writer.add_summary(summary, epoch * total_batch + i)
+            x_batch = torch.from_numpy(x_batch).float()
+            y_batch = torch.from_numpy(y_batch)
+            rep = encoder(x_batch)
+            print('rep',rep)
+            recon = decoder(rep)
+            print('recon',recon)
+            pred = learner(rep)
+            print('pred',pred)
+            # pred_from_rep = learner(R)
+
+            model_loss = criterion(pred,y_batch)
+            print('model_loss',model_loss)
+            recon_loss = criterion(x_batch,recon)
+            print('recon_loss',recon_loss)
+            loss_op = model_loss + recon_weight * recon_loss
+            print('loss_op',loss_op)
+            loss_op.backward()
+            optimizer.step()
+    
+
+            exit()
+
+if __name__ == "__main__":
+    batch_size = 4
+    x = np.random.random((batch_size*20,100))
+    y = np.random.random((batch_size*20,))
+    train_reg(x,y,batch_size=batch_size)
